88945e2c105f9ef3384c6379ec30bef5be406205
ENvironmentSet <sbshw1@gmail.com>
2024-11-02 15:19:06 +0900

    implement heading

--- src/Home/page.tsx
+++ src/Home/page.tsx
@@ -1,129 +1,87 @@
-import Header from '../Header/component.tsx'
 import { useState } from 'react'
-import {
-  bye,
-  catchphrase,
-  congrats, copyButton, footer,
-  headline,
-  headlineBox,
-  headlineSection,
-  introduction,
-  main, share, shareDescription,
-  showcase, spin, sticker, typography
-} from './style.css.ts'
-import clsx from 'clsx'
 
-import headlineLargeDesktop from '../assets/headline-large-desktop.png'
-import faceSticker from '../assets/face-sticker.png'
-import letterSticker from '../assets/letter-sticker.png'
-import heartSticker from '../assets/heart-sticker.png'
-import smileSticker from '../assets/smile-sticker.png'
-import catSticker from '../assets/cat-sticker.png'
+import Header from '../Header/component.tsx'
+import { heading, headline, headlineBox } from './style.css.ts'
 
-function Home() {
+import headline320x from '../assets/headline-320x.png'
+import headline480x from '../assets/headline-480x.png'
+import headline768x from '../assets/headline-768x.png'
+import headline1200x from '../assets/headline-1200x.png'
+import headline1920x from '../assets/headline-1920x.png'
+
+export default function Home() {
   return (
     <>
-      <section className={headlineSection}>
-        <Header/>
+      <section className={heading}>
+        <Header />
 
         <picture className={headlineBox}>
-          <img src={headlineLargeDesktop} alt='이 행운의 편지는 영국에서 최초로 시작되어...' className={headline}/>
+          <source
+            media='(max-width: 767px)'
+            srcSet={`
+              ${headline320x} 298w,
+              ${headline480x} 429w,
+            `}
+          />
+          <source
+            media='(min-width: 768px)'
+            srcSet={`
+              ${headline768x} 700w,
+              ${headline1200x} 1119w,
+              ${headline1920x} 1759w,
+            `}
+          />
+          <img src={headline1920x} alt='이 행운의 편지는 영국에서 최초로 시작되어...' className={headline} />
         </picture>
       </section>
-      <main className={main}>
-        <h2 className={catchphrase}>
+      <main>
+        <h2>
           행복을 전하는 메세지를 입력해보세요!
         </h2>
 
-        <p className={introduction} style={{marginTop: '8rem'}}>
+        <p>
           사람들과 주고받는 따뜻한 말들이 줄어들고, 함께하는 기쁨이 점점 사라져 가고 있는 것 같습니다.
           만약 조금 더 유쾌하고 재밌는 말들이 오간다면, 우리 사회가 좀 더 부드럽고 따뜻하고,
           말랑말랑하고 아카시아 꽃 향기 나는 곳이 될 수 있지 않을가요?
         </p>
-        <p className={introduction} style={{marginTop: '2rem'}}>
+        <p>
           그래서 만들었습니다. 경쾌하고 즐거운 메게지를 위한 서체 "러키비키 야-야"!
           리듬감 있는 두꺼운 획과 웃는 모양의 빈 공간이 특징인 이 서체를, 기쁨과 활기를 담은 글을 쓸 때 사용해 보세요.
         </p>
 
-        <span className={congrats}>축하해</span>
+        <span>축하해</span>
       </main>
-      <section className={showcase}>
-        <span className={typography.large} style={{ marginBottom: '-8.5rem' }}>럭키한걸</span>
-        <span className={typography.large} style={{letterSpacing: '-10%'}}>울랄랄라랄</span>
-        <span className={typography.medium} style={{marginBottom: '2.5rem'}}>그렇게 좋은 일이</span>
-        <span className={typography.medium} style={{marginBottom: '5rem'}}>생겼다니 엄청나다</span>
-        <span className={typography.small} style={{marginBottom: '2rem'}}>정말???!!!! 졸 투 더 업 한다고?</span>
-        <span className={typography.small} style={{marginBottom: '2rem'}}>축하해 앞으로 너에게</span>
-        <span className={typography.small}>좋은 일만 있길...</span>
+      <section>
+        <span>럭키한걸</span>
+        <span>울랄랄라랄</span>
+        <span>그렇게 좋은 일이</span>
+        <span>생겼다니 엄청나다</span>
+        <span>정말???!!!! 졸 투 더 업 한다고?</span>
+        <span>축하해 앞으로 너에게</span>
+        <span>좋은 일만 있길...</span>
 
-        <span className={bye}>안녕</span>
+        <span>안녕</span>
       </section>
       <Footer/>
-      <Sticker src={faceSticker} width='10vw' initialPosition={{ x: '12vw', y: '5rem' }} />
-      <Sticker src={smileSticker} width='60vw' initialPosition={{ x: '3vw', y: '20rem' }} />
-      <Sticker src={letterSticker} width='20vw' initialPosition={{ x: '48vw', y: '16rem' }} />
-      <Sticker src={heartSticker} width='20vw' initialPosition={{ x: '75vw', y: '40rem' }} opacity={0.5} />
-      <Sticker src={catSticker} width='45vw' initialPosition={{ x: '52vw', y: '180rem' }} opacity={0.5} />
     </>
   )
 }
-
-export default Home
 
 function Footer() {
   const [isCopied, setIsCopied] = useState(false)
 
   return (
-    <footer className={footer}>
-      <span className={share}>공유하기</span>
-      <span className={shareDescription}>이 사이트를 소중한 사람들에게 공유해보세요!</span>
+    <footer>
+      <span>공유하기</span>
+      <span>이 사이트를 소중한 사람들에게 공유해보세요!</span>
       <button
         onClick={() => {
           setIsCopied(true)
           navigator.clipboard.writeText('https://lucky-biki-ya-ya.vercel.app').catch(() => {
           })
         }}
-        className={clsx(copyButton, isCopied && spin)}
       />
-      <span className={shareDescription}
-            style={{height: '1.125rem', marginTop: '1.125rem', marginBottom: '7.56rem'}}>{isCopied && '복사되었습니다!'}</span>
+      <span>{isCopied && '복사되었습니다!'}</span>
     </footer>
   )
-}
-
-interface StickerProps {
-  src: string
-  initialPosition: {
-    x: string,
-    y: string,
-  }
-  width: string
-  opacity?: number
-}
-
-function Sticker({ src, initialPosition, width, opacity = 1 }: StickerProps) {
-  const [{ dx, dy }, setDelta] = useState({ dx: 0, dy: 0 })
-
-  return <img
-    src={src}
-    alt='sticker on page'
-    style={{ left: `calc(${initialPosition.x} + ${dx}px)`, top: `calc(${initialPosition.y} + ${dy}px)`, width, opacity }}
-    onMouseDown={event => {
-      const startX = event.pageX - dx
-      const startY = event.pageY - dy
-      const onMouseMove = (event: MouseEvent) => {
-        setDelta({
-          dx: event.pageX - startX,
-          dy: event.pageY - startY
-        })
-      }
-
-      document.addEventListener('mousemove', onMouseMove)
-      document.addEventListener('mouseup', () => {
-        document.removeEventListener('mousemove', onMouseMove)
-      }, { once: true })
-    }}
-    onDragStart={event => event.preventDefault()}
-    className={sticker}
-  />
 }
--- src/Home/style.css.ts
+++ src/Home/style.css.ts
@@ -1,147 +1,23 @@
-import { style, styleVariants, keyframes } from '@vanilla-extract/css'
-import { luckyVickyText, pretendardText } from '../typography.css.ts'
-import { SECONDARY, SHADOW, TYPOGRAPHY } from '../color.ts'
+import { style } from '@vanilla-extract/css'
 
-import copy from '../assets/copy.jpg'
+export const heading = style({
+  height: ['100dvh', '100vh'],
+  width: '100vw',
 
-export const headlineSection = style({
   display: 'flex',
   flexDirection: 'column',
-
-  height: ['100dvh', '100vh'],
 })
 
 export const headlineBox = style({
   flex: 1,
   alignSelf: 'center',
-  minHeight: 0,
+
+  minHeight: '0',
+  padding: '0 2.5rem',
 })
 
 export const headline = style({
   objectFit: 'contain',
-
   height: '100%',
-})
-
-export const main = style({
-  display: 'flex',
-  flexDirection: 'column',
-
-  marginTop: ['33dvh', '33vh'],
-})
-
-export const catchphrase = style([luckyVickyText, {
-  color: SECONDARY,
-  fontSize: '8rem',
-  textAlign: 'center',
-  lineHeight: 1.8,
-
-  padding: '0 14.625rem',
-
-  margin: 0,
-}])
-
-export const introduction = style([pretendardText, {
-  lineHeight: 1.8,
-  letterSpacing: '-1%',
-  fontSize: '1.125rem',
-  fontWeight: 600,
-
-  padding: '0 42rem',
-  marginBottom: 0,
-}])
-
-export const congrats = style([luckyVickyText, {
-  position: 'relative',
-  right: '6rem',
-
-  color: SHADOW,
-  fontSize: '18rem',
-
-  marginTop: '7rem',
-}])
-
-export const showcase = style({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'flex-start',
-
-  marginTop: '35rem',
-})
-
-export const typography = styleVariants({
-  large: [luckyVickyText, {
-    color: TYPOGRAPHY,
-    fontSize: '19.375rem',
-    lineHeight: 1.8,
-  }],
-  medium: [luckyVickyText, {
-    color: TYPOGRAPHY,
-    fontSize: '10rem',
-  }],
-  small: [luckyVickyText, {
-    color: TYPOGRAPHY,
-    fontSize: '5.25rem',
-    lineHeight: 1.6,
-  }]
-})
-
-export const bye = style([luckyVickyText, {
-  position: 'relative',
-  right: '10rem',
-
-  alignSelf: 'flex-end',
-
-  color: SHADOW,
-  fontSize: '16.15rem',
-}])
-
-export const footer = style({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-
-  marginTop: '22rem',
-})
-
-export const share = style([pretendardText, {
-  lineHeight: 1.8,
-  fontSize: '2rem',
-  fontWeight: 600,
-}])
-
-export const shareDescription = style([pretendardText, {
-  fontSize: '1.125rem',
-  fontWeight: 500,
-}])
-
-export const copyButton = style({
-  marginTop: '3.25rem',
-
-  backgroundImage: `url(${copy})`,
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: 'contain',
-  width: '5.5rem',
-  height: '5.5rem',
-
-  border: 'none',
-  padding: 0,
-  backgroundColor: 'transparent',
-})
-
-const rotate = keyframes({
-  '0%': { transform: 'rotate(0deg)' },
-  '100%': { transform: 'rotate(360deg)' }
-})
-
-export const spin = style({
-  animationName: rotate,
-  animationDuration: '1.5s'
-})
-
-export const sticker = style({
-  position: 'absolute',
-  zIndex: 1,
-
-  objectFit: 'contain'
+  maxWidth: '100%',
 })
