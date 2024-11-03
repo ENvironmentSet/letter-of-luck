f99cc28e3e18f107641c4f824078fcba0ba2261c
ENvironmentSet <sbshw1@gmail.com>
2024-11-03 20:43:18 +0900

    monkey patch

--- src/About/style.css.ts
+++ src/About/style.css.ts
@@ -3,7 +3,7 @@ import { BLACK, LETTER_BACKGROUND } from '../color.ts'
 import { luckyVickyText } from '../typography.css.ts'
 
 export const page = style({
-  height: ['100dvh', '100vh'],
+  height: ['100dvh', '85vh'],
 
   display: 'flex',
   flexDirection: 'column',
